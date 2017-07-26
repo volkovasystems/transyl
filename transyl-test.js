b8eb75b92cab678d4b2a4ac41dc81ca7688dccc5
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-26 17:18:31 +0800

    Updated test.

--- transyl-test.js
+++ transyl-test.js
@@ -4,12 +4,12 @@ const transyl = require( "./transyl.js" );
 
 let symbol = Symbol( "hello" );
 
-assert.equal( transyl( symbol, Symbol( "hello" ) ), true, "should be equal" );
+assert.equal( transyl( symbol, Symbol( "hello" ) ), true, "should be equal to true" );
 
-assert.equal( transyl( symbol, "hello", Symbol( "hello" ) ), true, "should be equal" );
+assert.equal( transyl( symbol, "hello", Symbol( "hello" ) ), true, "should be equal to true" );
 
-assert.equal( transyl( symbol, Symbol.for( "hello" ) ), true, "should be equal" );
+assert.equal( transyl( symbol, Symbol.for( "hello" ) ), true, "should be equal to true" );
 
-assert.equal( transyl( symbol, Symbol.for( "hi" ) ), false, "should be equal" );
+assert.equal( transyl( symbol, Symbol.for( "hi" ) ), false, "should be equal to false" );
 
 console.log( "ok" );
